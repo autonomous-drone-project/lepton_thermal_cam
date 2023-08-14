09e36dd5cb0f80aa51dd575f962d2329d18c2196
Johan Rocha <johanmarcos50@gmail.com>
2023-08-14 20:03:39 -0300

    funcional

--- scripts/thermal_cam_sub.py
+++ scripts/thermal_cam_sub.py
@@ -1,72 +1,199 @@
-#!/usr/bin/python3
-# Description:
-# - Subscribes to real-time streaming video from your built-in webcam.
-#
-# Author:
-# - Wenderson Ferreira
- 
-# Import the necessary libraries
-import rospy # Python library for ROS
-from sensor_msgs.msg import Image # Image is the message type
-from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
-import cv2 # OpenCV library
-import time # Time library
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-def callback(data):
- 
-  # Used to convert between ROS and OpenCV images
-  br = CvBridge()
- 
-  # Output debugging information to the terminal
-  rospy.loginfo("receiving thermal frame")
-   
-  # Convert ROS Image message to OpenCV image
-  current_frame = br.imgmsg_to_cv2(data)
+from uvctypes import *
+import time
+import cv2
+import numpy as np
+try:
+  from queue import Queue
+except ImportError:
+  from Queue import Queue
+import platform
+import rospy
+from mavros_msgs.msg import State
+
+BUF_SIZE = 2
+q = Queue(BUF_SIZE)
+current_state = 'STABILIZE'
+
+def py_frame_callback(frame, userptr):
+
+  array_pointer = cast(frame.contents.data, POINTER(c_uint16 * (frame.contents.width * frame.contents.height)))
+  data = np.frombuffer(
+    array_pointer.contents, dtype=np.dtype(np.uint16)
+  ).reshape(
+    frame.contents.height, frame.contents.width
+  ) # no copy
+
+  #data = np.fromiter(
+  #   frame.contents.data, dtype=np.dtype(np.uint8), count=frame.contents.data_bytes
+  #).reshape(
+  #   frame.contents.height, frame.contents.width, 2
+  #) # copy
+
+  if frame.contents.data_bytes != (2 * frame.contents.width * frame.contents.height):
+    return
+
+  if not q.full():
+    q.put(data)
+
+PTR_PY_FRAME_CALLBACK = CFUNCTYPE(None, POINTER(uvc_frame), c_void_p)(py_frame_callback)
+
+def ktof(val):
+  return (1.8 * ktoc(val) + 32.0)
+
+def ktoc(val):
+  return (val - 27315) / 100.0
+
+def raw_to_8bit(data):
+  cv2.normalize(data, data, 0, 65535, cv2.NORM_MINMAX) #65535 
+  np.right_shift(data, 8, data)
+  return cv2.cvtColor(np.uint8(data), cv2.COLOR_GRAY2RGB)
+
+def display_temperature(img, val_k, loc, color):
+  val = ktoc(val_k)
+  cv2.putText(img,"{0:.1f} degC".format(val), loc, cv2.FONT_HERSHEY_SIMPLEX, 0.75, color, 2)
+  x, y = loc
+  cv2.line(img, (x - 2, y), (x + 2, y), color, 1)
+  cv2.line(img, (x, y - 2), (x, y + 2), color, 1)
+
+def get_devices(ctx):
+
+  devices = []
+
+  devs = POINTER(c_void_p)()
+  res = libuvc.uvc_get_device_list(ctx, byref(devs))
+
+  if res != 0:
+    print("uvc_find_device error")
+    exit(1)
+
+  count = 0
+  while devs[count] != None:
+    dev = cast(devs[count], POINTER(uvc_device))
+    count += 1
+
+    desc = POINTER(uvc_device_descriptor)()
+    res = libuvc.uvc_get_device_descriptor(dev, byref(desc))
+
+    if res != 0:
+      print("Could not get device descriptor")
+      continue
+
+    if desc.contents.idProduct == PT_USB_PID and desc.contents.idVendor == PT_USB_VID:
+      devices.append((desc.contents, dev))
+
+  return devices
+
+# Callback function to handle state updates
+def main():
   
-  # so, convert them from float to integer.
-  frame_width = 80
-  frame_height = 60
-   
-  file_size = (frame_width, frame_height)
-  
-  hms = time.strftime('%H:%M:%S', time.localtime())
-  
-  file_path = '~/videos/thermal_' + str(hms) + '.avi'
-  
-  frames_per_second = 20
- 
-  # Display image
-  #cv2.imshow("camera", cv2.resize(current_frame, (640, 480)))
- 
-  fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-  video = cv2.VideoWriter(file_path,  
-                           fourcc, 
-                           frames_per_second, 
-                           file_size)
-                           
-  self.video = video
-  
-  self.video.write(current_frame)
-  
-  cv2.waitKey(1)
-      
-def receive_message():
- 
-  # Tells rospy the name of the node.
-  # Anonymous = True makes sure the node has a unique name. Random
-  # numbers are added to the end of the name. 
-  rospy.init_node('thermal_sub_py', anonymous=True)
-   
-  # Node is subscribing to the video_frames topic
-  rospy.Subscriber('thermal_frames', Image, callback)
- 
-  # spin() simply keeps python from exiting until this node is stopped
-  rospy.spin()
- 
-  self.video.release()
-  
-  # Close down the video stream when done
-  #cv2.destroyAllWindows()
-  
+    ctx = POINTER(uvc_context)()
+    ctrl = uvc_stream_ctrl()
+
+    res = libuvc.uvc_init(byref(ctx), 0)
+    if res < 0:
+      print("uvc_init error")
+      exit(1)
+
+    try:
+
+      devs = get_devices(ctx)
+
+      if len(devs) == 0:
+        print("Did not find any devices")
+        exit(1)
+
+      print("Found {} devices".format(len(devs)))
+
+      try:
+        second = False
+        for (desc, dev) in devs:
+          devh = POINTER(uvc_device_handle)()
+          res = libuvc.uvc_open(dev, byref(devh))
+          if res == 0:
+            break
+          print("could not open {}, trying next".format(desc.serialNumber))
+          second = True
+
+        if res < 0:
+          print("Could not open any devices")
+          exit(1)
+
+        print("device opened: ", desc.manufacturer, desc.product, desc.serialNumber)
+
+        print_device_info(devh)
+        print_device_formats(devh)
+
+        frame_formats = uvc_get_frame_formats_by_guid(devh, VS_FMT_GUID_Y16)
+        if len(frame_formats) == 0:
+          print("device does not support Y16")
+          exit(1)
+
+        libuvc.uvc_get_stream_ctrl_format_size(devh, byref(ctrl), UVC_FRAME_FORMAT_Y16,
+          frame_formats[0].wWidth, frame_formats[0].wHeight, int(1e7 / frame_formats[0].dwDefaultFrameInterval)
+        )
+
+        res = libuvc.uvc_start_streaming(devh, byref(ctrl), PTR_PY_FRAME_CALLBACK, None, 0)
+        
+        if res < 0:
+          print("uvc_start_streaming failed: {0}".format(res))
+          exit(1)
+        try:
+          writer = cv2.VideoWriter('novo.mp4', cv2.VideoWriter_fourcc(*'H264'), 20, (640, 480))
+          
+          mode_monitor(writer)
+          
+          writer.release()
+          cv2.destroyAllWindows()
+        finally:
+          libuvc.uvc_stop_streaming(devh)
+
+        print("done")
+      finally:
+        libuvc.uvc_unref_device(dev)
+    finally:
+      libuvc.uvc_exit(ctx)
+
+def callback_video(msg, writer):
+  global current_state
+  current_state = msg.mode
+    
+
+def mode_monitor(writer):
+    rospy.init_node('thermal_cam', anonymous=True)
+    global current_state
+
+    while not rospy.is_shutdown():
+      # Subscriber para o tópico que fornece o estado da aeronave
+
+      state_sub = rospy.Subscriber('/mavros/state', State, callback_video, writer)
+
+      if current_state == 'GUIDED':
+        rospy.init_node('thermal_cam', anonymous=True)
+
+        rospy.loginfo('Drone is in GUIDED mode')
+        data = q.get(True, 500)
+        if data is None:
+          return
+        data = cv2.resize(data[:,:], (640, 480)) #640x480
+        minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(data)
+        img = raw_to_8bit(data) ##funciona como cap   
+        im_color = cv2.applyColorMap(img, cv2.COLORMAP_JET) #image red and blue spectral
+        display_temperature(im_color, minVal, minLoc, (0, 0, 0))
+        display_temperature(im_color, maxVal, maxLoc, (255, 255, 255))
+
+        writer.write(im_color)
+        rospy.loginfo('successfully captured frame')
+
+    rospy.spin()
+
+
+
+
 if __name__ == '__main__':
-  receive_message()
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
